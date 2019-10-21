470bff20378a4a82616a85e133a07ef8846b6454
Dhanushkumar Sivaji <dhanushkumar.sivaji@gswvss.com>
2019-10-21 19:07:42 +0530

    added travis yml file

--- config/ci.js
+++ config/ci.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://Dhanush1995:S*dk1995@cluster0-shard-00-00-iork0.mongodb.net:27017,cluster0-shard-00-01-iork0.mongodb.net:27017,cluster0-shard-00-02-iork0.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+    mongoURI: 'mongodb://127.0.0.1:27017/contact-keeper',
     jwtSecret: "secret"
   };
   
